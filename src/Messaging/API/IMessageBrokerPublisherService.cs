8026bb52b378ddb382c799dcdd17c13b2477b592
Victor Chang <vicchang@nvidia.com>
2022-04-05 14:36:43 -0700

    Initial commit with RabbitMQ support

---
+++ src/Messaging/API/IMessageBrokerPublisherService.cs
@@ -0,0 +1,23 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Monai.Deploy.Messaging.Messages;
+
+namespace Monai.Deploy.Messaging
+{
+    public interface IMessageBrokerPublisherService
+    {
+        /// <summary>
+        /// Gets or sets the name of the storage service.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Publishes a message to the service.
+        /// </summary>
+        /// <param name="topic">Topic where the message is published to</param>
+        /// <param name="message">Message to be published</param>
+        /// <returns></returns>
+        Task Publish(string topic, Message message);
+    }
+}
