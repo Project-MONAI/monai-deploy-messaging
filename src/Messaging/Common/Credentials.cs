367ce0fddd1b4b605c99fe226888f5b26520e7b9
Victor Chang <mocsharp@users.noreply.github.com>
2022-04-11 08:25:15 -0700

    Event validation & add Task related events (#7)
    * Rename Message to Event used in the message body.
    * Add validation to events and validate event messages on all properties recursively.
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    Signed-off-by: JP LEGER <jeanpatrick.leger@gmail.com>

---
+++ src/Messaging/Common/Credentials.cs
@@ -0,0 +1,26 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.Messaging.Common
+{
+    public class Credentials
+    {
+        /// <summary>
+        /// Access key or username of the credentials pair.
+        /// </summary>
+        [JsonProperty(PropertyName = "access_key")]
+        [Required]
+        public string? AccessKey { get; set; }
+
+        /// <summary>
+        /// Access token or password of the credentials pair.
+        /// </summary>
+        [JsonProperty(PropertyName = "access_token")]
+        [Required]
+        public string? AccessToken { get; set; }
+
+    }
+}
---
+++ src/Messaging/Events/TaskDispatchEvent.cs
@@ -0,0 +1,60 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using System.ComponentModel.DataAnnotations;
+using Monai.Deploy.Messaging.Common;
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.Messaging.Events
+{
+    public class TaskDispatchEvent : EventBase
+    {
+        /// <summary>
+        /// Gets or sets the ID representing the instance of the workflow.
+        /// </summary>
+        [JsonProperty(PropertyName = "workflow_id")]
+        [Required]
+        public string? WorkflowId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID representing the instance of the Task.
+        /// </summary>
+        [Required]
+        [JsonProperty(PropertyName = "task_id")]
+        public string? TaskId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the correlation ID.
+        /// </summary>
+        [JsonProperty(PropertyName = "correlation_id")]
+        [Required]
+        public string? CorrelationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the fully qualified assembly name of the task plug-in for the task.
+        /// </summary>
+        [JsonProperty(PropertyName = "task_assembly_name")]
+        [Required]
+        public string? TaskAssemblyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the task execution arguments.
+        /// </summary>
+        [JsonProperty(PropertyName = "arguments")]
+        public Dictionary<string, object>? Arguments { get; set; }
+
+        /// <summary>
+        /// Gets or sets the input storage information.
+        /// </summary>
+        [JsonProperty(PropertyName = "input")]
+        [Required]
+        public Storage? Input { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output storage information.
+        /// </summary>
+        [JsonProperty(PropertyName = "output")]
+        [Required]
+        public Storage? Output { get; set; }
+    }
+}
