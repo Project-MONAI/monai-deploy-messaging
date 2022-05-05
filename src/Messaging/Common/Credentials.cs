d6398fd8de770f5d571ad723ee1e922c435f1331
Victor Chang <mocsharp@users.noreply.github.com>
2022-05-05 12:22:45 -0700

    Make credentials optional in TaskDispatchEvent.Storage (#17)
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- src/Messaging/Common/Credentials.cs
+++ src/Messaging/Common/Credentials.cs
@@ -9,24 +9,30 @@ namespace Monai.Deploy.Messaging.Common
     public class Credentials
     {
         /// <summary>
-        /// Access key or username of the credentials pair.
+        /// Gets or sets the access key or user name of the credentials pair.
         /// </summary>
         [JsonProperty(PropertyName = "access_key")]
         [Required]
-        public string? AccessKey { get; set; }
+        public string AccessKey { get; set; }
 
         /// <summary>
-        /// Access token or password of the credentials pair.
+        /// Gets or sets the access token or password of the credentials pair.
         /// </summary>
         [JsonProperty(PropertyName = "access_token")]
         [Required]
-        public string? AccessToken { get; set; }
+        public string AccessToken { get; set; }
 
         /// <summary>
-        /// Session token of the credentials pair.
+        /// Gets or sets the session token of the credentials pair.
         /// </summary>
         [JsonProperty(PropertyName = "session_token")]
-        public string? SessionToken { get; set; }
+        public string SessionToken { get; set; }
 
+        public Credentials()
+        {
+            AccessKey = string.Empty;
+            AccessToken = string.Empty;
+            SessionToken = string.Empty;
+        }
     }
 }
