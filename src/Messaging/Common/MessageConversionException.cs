b8e1f98102e92584d65b03a966ad0b13af00c40d
Victor Chang <mocsharp@users.noreply.github.com>
2022-05-17 10:14:46 -0700

    Throw MessageConversionException on error converting JSON event message (#22)
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

---
+++ src/Messaging/Common/MessageConversionException.cs
@@ -0,0 +1,27 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+//
+using System.Runtime.Serialization;
+
+namespace Monai.Deploy.Messaging.Common
+{
+    [Serializable]
+    public class MessageConversionException : Exception
+    {
+        public MessageConversionException()
+        {
+        }
+
+        public MessageConversionException(string message) : base(message)
+        {
+        }
+
+        public MessageConversionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MessageConversionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
