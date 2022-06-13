bd913b31d5e0444e7ace8944f014bf83a9dcefa3
jeanpatrickleger <jeanpatrick.leger@gmail.com>
2022-06-13 11:34:09 -0500

    Options to enable secure RabbitMQ (#27)
    * RMQ connection support for SSL/TLS (no certificate check).
    * Refactored RabbitMqConnectionFactory to support TLS connection and Port number config.
    * Update on IRabbitMqConnectionFactory Summary.
    
    Co-authored-by: JP Leger <jpleger@amazon.com>

--- src/Messaging/RabbitMq/RabbitMqMessageSubscriberService.cs
+++ src/Messaging/RabbitMq/RabbitMqMessageSubscriberService.cs
@@ -19,6 +19,8 @@ namespace Monai.Deploy.Messaging.RabbitMq
         private readonly string _endpoint;
         private readonly string _virtualHost;
         private readonly string _exchange;
+        private readonly string _useSSL = string.Empty;
+        private readonly string _portNumber = string.Empty;
         private readonly IModel _channel;
         private bool _disposedValue;
 
@@ -40,8 +42,18 @@ namespace Monai.Deploy.Messaging.RabbitMq
             _virtualHost = configuration.SubscriberSettings[ConfigurationKeys.VirtualHost];
             _exchange = configuration.SubscriberSettings[ConfigurationKeys.Exchange];
 
+   
+            if (configuration.SubscriberSettings.ContainsKey(ConfigurationKeys.UseSSL))
+                _useSSL = configuration.SubscriberSettings[ConfigurationKeys.UseSSL];
+
+                
+
+            if (configuration.SubscriberSettings.ContainsKey(ConfigurationKeys.Port))
+                _portNumber = configuration.SubscriberSettings[ConfigurationKeys.Port];
+         
+
             _logger.ConnectingToRabbitMq(Name, _endpoint, _virtualHost);
-            _channel = rabbitMqConnectionFactory.CreateChannel(_endpoint, username, password, _virtualHost);
+            _channel = rabbitMqConnectionFactory.CreateChannel(_endpoint, username, password, _virtualHost , _useSSL , _portNumber);
             _channel.ExchangeDeclare(_exchange, ExchangeType.Topic, durable: true, autoDelete: false);
             _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
         }
