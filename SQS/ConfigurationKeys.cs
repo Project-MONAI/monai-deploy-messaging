63359a3b9fbc66c4d38f7de4ac592ce5a8f3655a
JP LEGER <jeanpatrick.leger@gmail.com>
2022-06-14 08:20:27 -0500

    SQS Support adde to messaging lib

---
+++ SQS/ConfigurationKeys.cs
@@ -0,0 +1,19 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+namespace Monai.Deploy.Messaging.Configuration
+{
+    internal static class SQSConfigurationKeys
+    {
+        public static readonly string AccessKey = "accessKey";
+        public static readonly string AccessToken = "accessToken";
+        public static readonly string Region = "region";
+        public static readonly string WorkflowRequestQueue = "workflowRequestQueue";
+        public static readonly string ExportRequestQueue = "exportRequestQueue";
+        public static readonly string BucketName = "bucketName";
+        public static readonly string Envid = "environmentId";
+
+        public static readonly string[] PublisherRequiredKeys = new[] { WorkflowRequestQueue, BucketName };
+        public static readonly string[] SubscriberRequiredKeys = new[] { ExportRequestQueue, BucketName };
+    }
+}
---
+++ SQS/QueueFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Monai.Deploy.Messaging.SQS
+{
+    internal static class QueueFormatter
+    {
+        /// <summary>
+        /// Returns an aggregate of the the environmentId, queueBasename nd topic as the name of the queue defined in SQS.
+        /// The returned string is made compliant to SQS naming convention : It will replace non alphanumeric and other characters than "_" and "-", by an hyphen
+        /// </summary>
+        /// <param name="environmentId"></param>
+        /// <param name="queuebasename"></param>
+        /// <param name="topic"></param>
+        /// <returns>string</returns>
+        public static string FormatQueueName(string environmentId, string? queuebasename, string topic)
+        {
+
+            string queue = $"{queuebasename}_{topic}";
+
+            if (!environmentId.Equals(String.Empty))
+                queue = $"{environmentId}_{queue}";
+            queue = Regex.Replace(queue, "[^a-zA-Z0-9_]", "-");
+            if (queue.Length > 80)
+                queue = queue.Substring(0, 80);
+            return queue;
+        }
+    }
+}
