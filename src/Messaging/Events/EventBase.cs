bf9d31ce9003d93b49a6d95820e33b0c35251084
Lillie Dae <61380713+lillie-dae@users.noreply.github.com>
2022-07-20 09:51:37 +0100

    Try validate recursively fix (#43)
    * fix to resolve TryValidateRecursively recursively parsing dicts and object types
    
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/Messaging/Events/EventBase.cs
+++ src/Messaging/Events/EventBase.cs
@@ -31,13 +31,13 @@ namespace Monai.Deploy.Messaging.Events
         {
             var validationContextItems = new Dictionary<object, object?>();
             var validationResults = new List<ValidationResult>();
-            if (!TryValidateRecusively(this, validationContextItems, validationResults, new HashSet<object>(), GetType().Name))
+            if (!TryValidateRecursively(this, validationContextItems, validationResults, new HashSet<object>(), GetType().Name))
             {
                 throw new MessageValidationException(validationResults);
             }
         }
 
-        private bool TryValidateRecusively<T>(T instance,
+        private bool TryValidateRecursively<T>(T instance,
                                               IDictionary<object, object?> validationContextItems,
                                               List<ValidationResult> validationResults,
                                               ISet<object> validatedObjects,
@@ -48,6 +48,11 @@ namespace Monai.Deploy.Messaging.Events
             Guard.Against.Null(validationResults, nameof(validationResults));
             Guard.Against.Null(validatedObjects, nameof(validatedObjects));
             Guard.Against.NullOrWhiteSpace(propertyPath, nameof(propertyPath));
+
+            if (instance.GetType().IsGenericType)
+            {
+                return true;
+            }
 
             if (validatedObjects.Contains(instance))
             {
@@ -96,7 +101,7 @@ namespace Monai.Deploy.Messaging.Events
             }
 
             var nestedValidationResults = new List<ValidationResult>();
-            if (!TryValidateRecusively(value, validationContextItems, nestedValidationResults, validatedObjects, propertyPath))
+            if (!TryValidateRecursively(value, validationContextItems, nestedValidationResults, validatedObjects, propertyPath))
             {
                 result = false;
                 foreach (var validationResult in nestedValidationResults)
@@ -126,7 +131,7 @@ namespace Monai.Deploy.Messaging.Events
                 if (enumObj is not null)
                 {
                     var nestedValidationResults = new List<ValidationResult>();
-                    if (!TryValidateRecusively(enumObj, validationContextItems, nestedValidationResults, validatedObjects, propertyPath))
+                    if (!TryValidateRecursively(enumObj, validationContextItems, nestedValidationResults, validatedObjects, propertyPath))
                     {
                         result = false;
                         foreach (var validationResult in nestedValidationResults)
