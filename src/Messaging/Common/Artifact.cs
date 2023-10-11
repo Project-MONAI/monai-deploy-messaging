8fec32e0524e3a1f6735705c0ed54d367edb5e54
Neil South <neil.south@answerdigital.com>
2023-10-11 11:28:40 +0100

    adding new ArtifactsReceivedEvent
    Signed-off-by: Neil South <neil.south@answerdigital.com>

---
+++ src/Messaging/Common/Artifact.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Monai.Deploy.Messaging.Common
+{
+    public class Artifact
+    {
+        public string Type { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+    }
+}
